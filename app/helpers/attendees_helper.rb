fb3f544f1cee9f86c25378cc9d4aabe7077b41c4
Zhang Yuanyi <yzhang@elctech.com>
2009-10-19 09:52:36 +0800

    update price algorithm

--- app/helpers/attendees_helper.rb
+++ app/helpers/attendees_helper.rb
@@ -1,14 +1,15 @@
 module AttendeesHelper
-  def total_fee(attendee)                       
-    unless attendee.number > 1
-     attendee.join_party ? (discount_price(TICKET_PRICE) + 5000) : TICKET_PRICE
-   else
-     attendee.join_party ? (discount_price(TICKET_PRICE)*0.8 + 5000)*attendee.number : (TICKET_PRICE * attendee.number)
-   end  
+  def total_fee(attendee)
+    price = discount_price(TICKET_PRICE)
+    if attendee.number > 1
+     attendee.join_party ? (price*0.8 + 5000) * attendee.number : (price*0.8 * attendee.number)
+    else
+      attendee.join_party ? (price + 5000) : price
+    end  
   end  
   
   def discount_price(price)
-    new_price = Date.today < '2009-10-21'.to_date ?  (price - 2000) : price
+    new_price = (Date.today < '2009-10-21'.to_date ?  (price - 2000) : price)
   end
   
 end
